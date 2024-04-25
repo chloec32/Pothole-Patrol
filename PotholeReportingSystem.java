6ebd0063cc89a39c3ea5d256449e262536cc5f4a
chloec32 <102554051+chloec32@users.noreply.github.com>
2024-04-25 13:00:42 -0400

    Update PotholeReportingSystem.java

--- PotholeReportingSystem.java
+++ PotholeReportingSystem.java
@@ -1,3 +1,5 @@
 // Creates PotholeReports from Citizen Reports
 // Receieves Updates on Potholes from Maintenance System
 // Displays Potholes for Citizens and Public Works Director
+
+import PotholeReport;
