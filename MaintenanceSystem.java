a9e9f122ba6335f87623f242e9e5fa7981cb7fad
chloec32 <102554051+chloec32@users.noreply.github.com>
2024-04-25 13:17:00 -0400

    Update MaintenanceSystem.java

--- MaintenanceSystem.java
+++ MaintenanceSystem.java
@@ -5,4 +5,14 @@
 // Receives Updated WorkOrders from MaintenanceDepartment
 // Sends WorkOrder Updates to PotholeReportingSystem
 
-import PotholeReport;
+import WorkOrder;
+import java.time.*;
+
+
+System.out.println(public int getHour());
+
+if (public int getHour() == 6) {
+  // Beginning of Day
+}
+
+
