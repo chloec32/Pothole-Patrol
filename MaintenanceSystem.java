fb42b48b8bf4489a4063a16d9f084d3aa7853a26
chloec32 <102554051+chloec32@users.noreply.github.com>
2024-04-25 13:05:47 -0400

    Create MaintenanceSystem.java

---
+++ MaintenanceSystem.java
@@ -0,0 +1,8 @@
+// Receieves PotholeReports and Creates WorkOrders from them
+// adds RepPrioirity, RepairStatus, EstRepairTime
+
+// Sends WorkOrders to MaintenanceDepartment
+// Receives Updated WorkOrders from MaintenanceDepartment
+// Sends WorkOrder Updates to PotholeReportingSystem
+
+import PotholeReport;
